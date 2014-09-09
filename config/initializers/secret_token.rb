2b53c887ec0d6f74e2c8a306253230c58dec1079
eszkarolina <eszkarolina@gmail.com>
2014-09-09 22:33:50 +0200

    :circus_tent: Added .gitattributes

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Railsgirls::Application.config.secret_key_base = 'b5053e2e148578aace04334ea8a3a6c83052b7bf35dc6172e2754914fd57f5ba9eeb5bca9c87a65536e0fabbcd6f14a3e7f8337bb56291e5de87429659a211cd'
